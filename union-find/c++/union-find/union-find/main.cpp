ef566c32539f788a52f1fe3cfc72c055c1ac6c7c
sambot <sambot1981@gmail.com>
2015-01-28 18:27:53 +0200

    test boost

--- union-find/c++/union-find/union-find/main.cpp
+++ union-find/c++/union-find/union-find/main.cpp
@@ -1,45 +1,8 @@
-
-
 #include <iostream>
-//#include <iterator>
-//#include <algorithm>
-//#include <boost/lambda/lambda.hpp>
-//#include <boost/current_function.hpp >
-//#include <boost/foreach.hpp >
-//#include <boost/static_assert.hpp >
-//#include <boost/detail/lightweight_test.hpp >
-
-//using namespace std;
-
-//# include < vector >
-//
-//# define MY_NUM 1300
-//void testMacroa(){
-//	std::cout << " You have called :" << BOOST_CURRENT_FUNCTION << std::endl;
-//}
-//
-//void testMacrob(){
-//	BOOST_STATIC_ASSERT(MY_NUM != 1400);
-//}
-//void testMacroc(){
-//	std::vector < double > myVec(10);
-//	BOOST_FOREACH(double & x, myVec) x = 10.0;
-//	BOOST_FOREACH(double x, myVec) std::cout << x << std::endl;
-//}
-//void testMacrod(){
-//	BOOST_ERROR(" Failure of this function ");
-//	double x = 0.0;
-//	BOOST_TEST(x != 0);
-//}
-
+#include "QF.h"
 
 int main(){
 
-	//testMacroa();
-	//testMacrob();
-	//testMacroc();
-	//testMacrod();
-	std::cout << "HelloWorld" << std::endl;
-
+	QF *p = &QF(10);
 	system("pause");
 }
